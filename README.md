8d63e82518674ce786309e0e19971c2c859da601
Alin Grigorescu <alin.grig87@gmail.com>
2023-04-26 21:15:55 +0300

    2. Tasks should be displayed below the input field in an unordered list. 3. Each task should have a Delete button that allows the task to be removed from the list. 4. After a new task has been added, the input field should be cleared.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TODO List - DOM Manipulation
 
 1. When the user enters a new task in the input field, a "Add" button should be displayed to allow adding the task to the list.
-2. Tasks should be displayed below the input field in an ordered list.
+2. Tasks should be displayed below the input field in an unordered list.
 3. Each task should have a "Delete" button that allows the task to be removed from the list.
 4. After a new task has been added, the input field should be cleared.
 5. After a new task has been added, the "Add" button should be disabled until the user enters another task in the input field.
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const todoInput = document.getElementById('todo-input');
 const addTodoBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
 
 todoInput.addEventListener('input', onInputTodo);
+addTodoBtn.addEventListener('click', onAddTodo);
 
 function onInputTodo(event) {
 	if (event.target.value.length > 0) {
@@ -9,4 +11,25 @@ function onInputTodo(event) {
 	} else {
 		addTodoBtn.setAttribute('disabled', true);
 	}
+}
+
+function onAddTodo() {
+	const todoLi = createTodoLi(todoInput.value);
+	todoList.appendChild(todoLi);
+	todoInput.value = '';
+	addTodoBtn.setAttribute('disabled', true);
+}
+
+function createTodoLi(text) {
+	const todoLi = document.createElement('li');
+	const todoTextSpan = document.createElement('span');
+	const deleteTodoBtn = document.createElement('button');
+
+	todoTextSpan.innerHTML = text;
+	deleteTodoBtn.innerHTML = 'X';
+
+	todoLi.appendChild(todoTextSpan);
+	todoLi.appendChild(deleteTodoBtn);
+
+	return todoLi;
 }
